d27e32c7f6390009a6cb637ff27f04629a55107e
Sean Grainger <ninqt@protonmail.com>
2026-06-23 17:15:00 +1000

    UserAdapter created

---
+++ backend/adapters/userAdapter.js
@@ -0,0 +1,20 @@
+class UserAdapter {
+
+    static adapt(user) {
+        return {
+
+
+            name: user.name,
+
+            email: user.email,
+
+            password: user.password,
+
+            admin: user.admin ?? false,
+
+            critic: user.critic ?? false,
+
+        }
+    }
+
+}
