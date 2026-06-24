02c2ae5edc88b82aa4c459b6ae4186540a9b22c3
Sean Grainger <ninqt@protonmail.com>
2026-06-24 22:03:43 +1000

    Created ReviewBuilder

--- backend/services/reviewService.js
+++ backend/services/reviewService.js
@@ -19,3 +19,20 @@ class ReviewFactory{
 }
 
 
+class ReviewBuilder{
+    static buildReview(review,user){
+        this.setCriticReview(review,user)
+        this.setDateTime(review)
+        this.setHighlightable(review)
+    }
+    static setCriticReview(review,user){
+        review.isCriticReview = user.critic
+    }
+    static setDateTime(review){
+        review.date = new Date().toLocaleDateString
+        review.time = new Date().toLocaleTimeString
+    }
+    static setHighlightable(review){
+        review.isHighlightable = (review.description.trim().split(/\s+/).length || 0) >= 50
+    }
+}
